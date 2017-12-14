88625140a67ae3d520b9a34013f478376614d96d
CARON Louis <louis.caron@renault.com>
2017-12-14 14:58:35 +0100

    Fix possible empty register, add configurable scopes, add tap state info

--- jtag_parse.py
+++ jtag_parse.py
@@ -30,9 +30,10 @@ class JTAGWatcher(watcher.VcdWatcher):
         self.add_watching(self.signame_tdi)
         self.add_watching(self.signame_tdo)
 
-    def set_writer(self, writer, timescale, var):
+    def set_writer(self, writer, timescale, statevar, opvar):
         self.writer = writer
-        self.var = var
+        self.statevar = statevar
+        self.opvar = opvar
         self.timescale = timescale
 
     def update_ids(self):
@@ -68,22 +69,23 @@ class JTAGWatcher(watcher.VcdWatcher):
 class JTAGTracker(tracker.VcdTracker):
     def start(self):
         # called at the creation of the tracker
-        self.op_start = 0
+        pass
 
     def update(self):
+        prevstate = self.watcher.curstate
         # retrieve the current state in the watcher and execute state action
         getattr(self, self.watcher.curstate)()
+        if prevstate != self.watcher.curstate:
+            self.watcher.writer.change(self.watcher.statevar, self.parser.now, self.watcher.curstate)
 
     def test_logic_reset(self):
         tms = int(self.values[self.watcher.id_tms])
         if tms == 0:
-            self.watcher.writer.change(self.watcher.var, self.parser.now, 'idle')
             self.watcher.curstate = 'run_test_idle'
 
     def run_test_idle(self):
         tms = int(self.values[self.watcher.id_tms])
         if tms == 1:
-            self.op_start = self.parser.now
             self.watcher.curstate = 'select_dr_scan'
 
     def select_dr_scan(self):
@@ -129,24 +131,28 @@ class JTAGTracker(tracker.VcdTracker):
             self.watcher.curstate = 'shift_dr'
 
     def update_dr(self):
+        if self.watcher.dr_i != '':
+            print(str(self.parser.now) + ":")
+            dr_i = hex(int('0b' + self.watcher.dr_i, 0))
+            dr_o = hex(int('0b' + self.watcher.dr_o, 0))
+            print('   in : ' + self.watcher.dr_i + '(' + dr_i + ')')
+            print('   out: ' + self.watcher.dr_o + '(' + dr_o + ')')
+            s = 'in=' + dr_i + '-out=' + dr_o
+        else:
+            # this can happen in the path: dr-scan -> capture-dr -> exit1-dr -> update-dr
+            s = 'in=NULL-out=NULL'
+        self.watcher.writer.change(self.watcher.opvar, self.parser.now, s)
+
         tms = int(self.values[self.watcher.id_tms])
-        print('instruction ' + hex(self.watcher.last_ir))
-        dr_i = hex(int('0b' + self.watcher.dr_i, 0))
-        dr_o = hex(int('0b' + self.watcher.dr_o, 0))
-        print('   in  ' + dr_i)
-        print('   out ' + dr_o)
-        self.watcher.writer.change(self.watcher.var, self.op_start, 'in='+dr_i+',out='+dr_o)
         if tms == 1:
-            self.op_start = self.parser.now
             self.watcher.curstate = 'select_dr_scan'
         else:
-            self.watcher.writer.change(self.watcher.var, self.parser.now, 'idle')
             self.watcher.curstate = 'run_test_idle'
 
     def select_ir_scan(self):
         tms = int(self.values[self.watcher.id_tms])
         if tms == 1:
-            self.watcher.writer.change(self.watcher.var, self.parser.now, 'reset')
+            self.watcher.writer.change(self.watcher.opvar, self.parser.now, 'reset')
             self.watcher.curstate = 'test_logic_reset'
         else:
             self.watcher.curstate = 'capture_ir'
@@ -185,14 +191,19 @@ class JTAGTracker(tracker.VcdTracker):
             self.watcher.curstate = 'shift_ir'
 
     def update_ir(self):
+        if self.watcher.ir_i != '':
+            ir = int('0b' + self.watcher.ir_i, 0)
+            s = 'ir=' + self.watcher.ir_i + '(' + hex(ir) + ')'
+        else:
+            # this can happen in the path: ir-scan -> capture-ir -> exit1-ir -> update-ir
+            s = 'ir=NULL'
+        self.watcher.writer.change(self.watcher.opvar, self.parser.now, s)
+        print(str(self.parser.now) + ': ' + s)
+
         tms = int(self.values[self.watcher.id_tms])
-        self.watcher.last_ir = int('0b' + self.watcher.ir_i, 0)
-        self.watcher.writer.change(self.watcher.var, self.op_start, 'ir='+hex(self.watcher.last_ir))
         if tms == 1:
-            self.op_start = self.parser.now
             self.watcher.curstate = 'select_dr_scan'
         else:
-            self.watcher.writer.change(self.watcher.var, self.parser.now, 'idle')
             self.watcher.curstate = 'run_test_idle'
 
 
@@ -216,18 +227,21 @@ argparser.add_argument('-s', '--initstate', choices=tap_states, default=tap_stat
     help='initial tap controller state')
 argparser.add_argument('-t', '--timescale', choices=timescales, default='1 ns',
     help='timescale to match input file')
-argparser.add_argument('--scope', default='parsed',
-    help='scope of the jtag signal')
+argparser.add_argument('--inscope', default='capture',
+    help='scope of the jtag signals in the input file')
+argparser.add_argument('--outscope', default='parsed',
+    help='scope of the parsed information in the output file')
 
 my_args = argparser.parse_args()
 
 vcd = parser.VcdParser()
 
 with VCDWriter(my_args.outfile, timescale=my_args.timescale, date='today') as writer:
-    jtag_v = writer.register_var(my_args.scope, 'jtag', 'string', init=my_args.initstate)
+    tapstate_v = writer.register_var(my_args.outscope, 'tap_state', 'string', init=my_args.initstate)
+    jtag_v = writer.register_var(my_args.outscope, 'jtag', 'string', init=my_args.initstate)
 
-    w = JTAGWatcher('capture', my_args.tck, my_args.tms, my_args.tdi, my_args.tdo, my_args.initstate)
-    w.set_writer(writer, my_args.timescale, jtag_v)
+    w = JTAGWatcher(my_args.inscope, my_args.tck, my_args.tms, my_args.tdi, my_args.tdo, my_args.initstate)
+    w.set_writer(writer, my_args.timescale, tapstate_v, jtag_v)
     w.set_tracker(JTAGTracker)
     vcd.register_watcher(w)
 
